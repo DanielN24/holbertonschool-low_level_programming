e7b9646a2eeb3fd9c8f81a18fb6d400627bc45e4
root <capdaniel_98@outlook.com>
2022-03-01 08:06:37 -0800

    new file with the answer number 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,8 +3,8 @@
 *_strchr - function that locates a character in a string
 *@s: pointer name
 *@c: variable name
-*Return: a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
-
+*Return: a pointer to the first occurrence of the character c in the string s
+*NULL if the character is not found
 */
 char *_strchr(char *s, char c)
 {
