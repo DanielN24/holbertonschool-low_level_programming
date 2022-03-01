5777694fa00d91f8c0ef016f4ea973f9440dd368
root <capdaniel_98@outlook.com>
2022-03-01 07:23:11 -0800

    new file with the answer number 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+**_memset - unction that fills memory with a constant byte
+*@s: pointer name 
+*Return: return s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
