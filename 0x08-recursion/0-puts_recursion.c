82d39e016528a2e32aeb525fd7150a20b6a961ce
root <capdaniel_98@outlook.com>
2022-03-02 09:12:20 -0800

    new file with the answer number 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_puts_recursion -  function that prints a string, followed by a new line.
+*@s: pointer name
+*/
+void _puts_recursion(char *s)
+{
+	if (!s || !*s)
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	_putchar('\n');
+}
