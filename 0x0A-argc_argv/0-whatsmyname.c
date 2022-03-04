6ada046da715543ccb3351bd4ded866830fd1be5
root <capdaniel_98@outlook.com>
2022-03-04 14:21:43 -0800

    new file with the answer number 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,8 @@
 *@argv: The program name
 *Return: always 0
 */
-int main (int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
