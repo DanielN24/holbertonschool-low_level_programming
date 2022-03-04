c93a6b86cc92ddc07f1fa43c49840bb72df57cbb
root <capdaniel_98@outlook.com>
2022-03-04 14:58:09 -0800

    new file with the answer number 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,11 +5,11 @@
 *@argv: The number of command line arguments
 *Return: always 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a;
 
 	for (a = 0; a < argc; a++)
 		printf("%s\n", argv[a]);
-	return(0);
+	return (0);
 }
