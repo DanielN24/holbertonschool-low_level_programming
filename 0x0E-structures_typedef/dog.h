c53c0fa8d8c78823b0de73c46230cd996e8af8dc
root <capdaniel_98@outlook.com>
2022-03-14 13:36:49 -0700

    new file with the answer number 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - Poppy
+*@name: name of poppy
+*@age: age of poppy
+*@owner: owner poppy
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
