f702dd2d9930b328ab7a3a4d65f9fe14a5abe603
root <capdaniel_98@outlook.com>
2022-03-14 10:27:53 -0700

    new file with the answer number 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H_
-#define OBJECT_LIKE_MARCO_H_
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H_ */
+#endif
