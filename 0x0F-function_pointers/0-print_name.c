62455d9fd8368e33b5f7088f10203d6f05062709
root <capdaniel_98@outlook.com>
2022-03-16 13:46:32 -0700

    new files with the project 0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*print_name - function that prints a name
+*@name: name to print
+*@f: pointer to function print
+*
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		exit(EXIT_SUCCESS);
+	f(name);
+}
