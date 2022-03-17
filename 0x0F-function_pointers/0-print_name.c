d89d9ca490ccb77676831fccb8e76cb30a539c1c
DanielN24 <capdaniel_98@outlook.com>
2022-03-17 07:00:06 -0700

    new file with the answer number 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,12 +3,12 @@
 *print_name - function that prints a name
 *@name: name to print
 *@f: pointer to function print
-*
+*Return: always 0
 */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-		exit(EXIT_SUCCESS);
+		return;
 
 	f(name);
 }
