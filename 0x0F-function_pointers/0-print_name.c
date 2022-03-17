c2adab7580d2f894a1f463af9d15d940b95e11ff
DanielN24 <capdaniel_98@outlook.com>
2022-03-17 07:02:35 -0700

    new file with the answer number 0 of the project

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 *print_name - function that prints a name
 *@name: name to print
 *@f: pointer to function print
-*Return: always 0
+*Return: always0
 */
 void print_name(char *name, void (*f)(char *))
 {
