fcf52152ddb625d8c690f670f3b43154251f6763
DanielN24 <capdaniel_98@outlook.com>
2022-03-17 06:51:45 -0700

    new file with the answer number 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 /**
 *print_name - function that prints a name
 *@name: name to print
@@ -10,5 +9,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		exit(EXIT_SUCCESS);
+
 	f(name);
 }
